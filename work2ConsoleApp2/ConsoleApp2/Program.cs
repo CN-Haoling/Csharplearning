578bb16d1d6536c02e7ce18faf3c3714f968b653
CN-Haoling <791331362@qq.com>
2020-05-26 11:14:12 +0800

    add

---
+++ work2ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 1, 3, 2, 4 ,0};
+            int count=0,size=array.Length;
+            int maxium=0, minium=65536;
+
+            for(count=0;count<size;count++)
+            { 
+                if (array[count] > maxium)
+                {   maxium = array[count];
+                        
+                }
+                if (array[count] < minium)
+                {
+                    minium = array[count];
+                }
+              
+            }
+            Console.WriteLine(maxium);
+            Console.WriteLine(minium);
+            Console.WriteLine(maxium);
+        }
+    }
+}
---
+++ work2ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit;
+            limit=Console.Read();
+            if (limit <= 0)
+            {
+                Console.WriteLine("Invalid input");//输入的int必须为正
+            }
+            int square(int x)
+            {
+
+                int sq;
+                int[] primes;
+                primes = new int [65536];
+                for (sq = 0; sq <= x / 2; sq++)
+                {
+                    if (sq * sq == x) { return sq; }
+                    else if (sq * sq < x&&(sq+1)*(sq+1)>x){ return sq; }
+                    else if (sq * sq > x) { return sq - 1; }
+
+
+                }
+                return 0;
+            }
+            for(int i = 2; i < square(limit); i++)
+            {
+                primes[i-2]
+            }
+
+        }`
+    }
+}
