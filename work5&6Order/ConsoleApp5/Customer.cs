12588106b581b516ef702e23335aac2742ab7051
CN-Haoling <791331362@qq.com>
2020-05-28 17:22:37 +0800

    order 5 6

---
+++ work5&6Order/ConsoleApp5/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    [Serializable]
+    public class Customer
+    {
+        private string customerName;
+        public string CustomerName
+        {
+            set { customerName = value; }
+            get { return customerName; }
+        }
+    }
+}
---
+++ work5&6Order/ConsoleApp5/OrderService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace ConsoleApp5 {
+
+    public class OrderService
+    {
+        private List<Order> totalOrder = new List<Order>();
+        public List<Order> TotalOrder
+        {
+            set { totalOrder = value; }
+            get { return totalOrder; }
+        }
+        public void ShowOrder(int OrderNum)
+        {
+            Order wantedOrder ;
+            if (totalOrder != null && totalOrder.Count > 0)
+            {
+                wantedOrder = FindOrder(OrderNum);
+                if (wantedOrder == null)
+                {
+                    Console.WriteLine(  OrderNum + "号订单不存在 ");
+                }
+                else
+                {
+                    OrderItem WantedOrder = new OrderItem(wantedOrder);
+                    Console.WriteLine("以下是订单号为：" + OrderNum + "的订单详情。");
+                    WantedOrder.getOrder();
+                }
+            }
+            else
+            {
+                Console.WriteLine("你还没有订单。");
+            }
+
+        }
+        public void DelectOrder(int OrderNum)
+        {
+            totalOrder.Remove(FindOrder(OrderNum));
+        }
+        public Order FindOrder(int OrderNum)
+        {
+            return totalOrder.Where(s => s.OrderNumber == OrderNum).First() as Order;
+        }
+        public void Export()
+        {
+            XmlSerializer xmlserializer = new XmlSerializer(typeof(List<Order>));
+            {
+                using (FileStream fs = new FileStream("s.xml", FileMode.Create))
+                {
+                    xmlserializer.Serialize(fs, totalOrder);
+                }
+            }
+        }
+        public void Import()
+        {
+            using (FileStream fs = new FileStream("s.xml", FileMode.Open))
+            {
+                XmlSerializer xmlserializer = new XmlSerializer(typeof(List<Order>));
+                totalOrder = (List<Order>)xmlserializer.Deserialize(fs);
+            }
+        }
+    }
+}
